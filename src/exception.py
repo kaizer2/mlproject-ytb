2dbbf6b79153b4952b73e21665914f9ca432edd2
kaizer2 <gillesgaetan76@gmail.com>
2025-06-22 23:00:21 +0200

    EDA in Notebook  and Problem statements

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys 
 import logging
+from src.logger import logging
+
 def error_message_detail(error, error_detail:sys):
    _,_,exc_tb=error_detail.exc_info()
    file_name=exc_tb.tb_frame.f_code.co_filename
@@ -22,3 +24,9 @@ class CustomException(Exception):
 
 
 
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys) 
